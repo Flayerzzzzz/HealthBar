ec0b3d1bf75825634eb6b6ac64a57232350366f2
Flayerzzzzz <ND_02@list.ru>
2022-09-15 03:33:33 +0300

    3
    3

---
+++ Assets/Scriptes/HealthVisualization.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthVisualization : MonoBehaviour
+{
+    [SerializeField] private Image _healthBar;
+    [SerializeField] private Health _health;
+
+    private Coroutine _healthRender;
+
+    private void Start()
+    {
+        _healthRender = StartCoroutine(HealthRender());
+    }
+
+    private void OnEnable()
+    {
+        _health._healthChanged += Render;
+    }
+
+    private void OnDisable()
+    {
+        _health._healthChanged -= Render;
+    }
+
+    private void Render()
+    {
+        if (_healthRender != null)
+        {
+            StopCoroutine(_healthRender);
+            _healthRender = StartCoroutine(HealthRender());
+        }
+    }
+
+    IEnumerator HealthRender()
+    {
+        while(true)
+        {
+            _healthBar.fillAmount = Mathf.Lerp(_healthBar.fillAmount, _health.HealthCount / _health.MaxHealth, Time.deltaTime);
+            yield return new WaitUntil(() => _healthBar.fillAmount != _health.HealthCount / _health.MaxHealth);
+        }
+    }
+}
+
